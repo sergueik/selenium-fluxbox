695c0b08c10fbe203d639582129b995fe6adc9dd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-24 02:41:47 +0100

    intermediate revision - added just arrived chrome version from "www.slimjet.com", updated download script debug / verbose behavior

--- get_chrome_versions.rb
+++ get_chrome_versions.rb
@@ -30,6 +30,7 @@ else
     end
     opts.on('-d', '--debug', 'Run in debug mode') do |data|
       options[:debug] = data
+
     end
     opts.on('-rRELEASE', '--release=RELEASE', 'Return the build number of closest match available for specific RELEASE of Chrome') do |data|
       options[:release] = data
@@ -41,7 +42,7 @@ end
 $DEBUG = options[:debug]
 if $DEBUG
   PP.pp options, $stderr
-  exit 0
+  # exit 0
 end
 $RELEASE = options[:release]
 $VERBOSE = options[:verbose]
@@ -53,14 +54,16 @@ has_nokogiri = false
 begin
   require 'nokogiri'
   if $DEBUG
-    puts 'Loading "nokogiri"'
+    $stderr.puts 'Loading "nokogiri"'
   end
   has_nokogiri = true
 rescue LoadError =>  e
   if ! has_nokogiri
     if $DEBUG
-      STDERR.printf 'Exception (ignored): %s', message
+      STDERR.printf 'Exception: %s', message
     end
+    $stderr.puts 'needed gem not found: nokogiri'
+    exit 1
   end
 end
 # TODO: clear
@@ -74,33 +77,32 @@ begin
 rescue LoadError =>  e
   if ! has_restlient
     if $DEBUG
-      # TODO:
-      # message = sprintf('Exception (ignored): %s', e.message)
-      STDERR.printf('Exception (ignored): %70.68s', e.message)
+      STDERR.printf('Exception: %70.68s', e.message)
     end
+    $stderr.puts 'needed gem not found: restclient'
   end
-end
-if $DEBUG
-  pp "Loaded nokogiri: #{has_nokogiri}"
-  pp "Loaded restclient: #{has_restclient}"
 end
 
 uri = URI('https://www.slimjet.com/chrome/google-chrome-old-version.php')
 req = Net::HTTP::Get.new(uri.path)
 downloads = {}
 res = Net::HTTP.start(
-        uri.host, uri.port,
-        :use_ssl => uri.scheme == 'https',
-        :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+  uri.host, uri.port,
+  :use_ssl => uri.scheme == 'https',
+  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
   res = https.request(req)
   if res.code.to_s == '200'
     input = res.body
-    if $VERBOSE
+    if $DEBUG
       STDERR.puts input
     end
+    # NOTE: cannot use xml tools: a big number of HTML parser error
     begin
       document = Nokogiri::HTML(input)
       document.search('a[href *= "download-chrome.php"][href *= ".deb"]').each do |row|
+        if $VERBOSE
+          $stderr.puts ('in row: ' + row)  
+        end   
         # release 69+ download url format change
         # from download-chrome.php?file=lnx%2Fchrome64_*.deb
         # to files%2F*%2Fgoogle-chrome-stable_current_amd64.deb
@@ -124,7 +126,7 @@ res = Net::HTTP.start(
   end
 end
 if $VERBOSE
-  pp downloads
+  PP.pp downloads, $stderr
 end
 if $RELEASE.nil?
   # TODO:
