7705d3f184f114f5f53a1d12a7347d831c76a55a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-04-03 12:25:24 -0400

    intermediate revision - added optparse and example usage

--- get_box_metadadata.rb
+++ get_box_metadadata.rb
@@ -5,18 +5,45 @@ require 'find'
 require 'json'
 require 'net/http'
 require 'openssl'
+require 'optparse'
 require 'pathname'
 require 'pp'
 require 'uri'
 
 @debug = false
+
+@options = {
+  :debug   => false,
+  :release => 'xenial64',
+  :distro  => 'ubuntu',
+}
+options_defined = false
+unless options_defined
+  o = OptionParser.new
+  o.on('--distro [DISTRO]', 'distro') do |val|
+    @options[:distro] = val
+  end
+
+  o.on('--release [RELEASE]', 'release') do |val|
+    @options[:release] = val
+  end
+
+  o.on('-d', '--debug', 'Debug') do |val|
+    @options[:debug] = true
+  end
+  o.parse!
+end
+pp @options
+release = @options[:release] || 'trusty64'
+distro = @options[:distro] || 'ubuntu'
+
 net = Net::HTTP.new('vagrantcloud.com', 443)
 net.use_ssl = true
 # https://www.engineyard.com/blog/ruby-ssl-error-certificate-verify-failed
 # certificate verify failed (OpenSSL::SSL::SSLError)
 net.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-response = net.get('/ubuntu/boxes/trusty64/')
+response = net.get("/#{distro}/boxes/#{release}/")
 location = response.response['location'].to_s
 if @debug
   $stderr.puts ('Body = ' + response.body)
@@ -36,3 +63,21 @@ end
 metadata = JSON.parse(response.body)
 # pp metadata
 pp metadata['versions'][0]['version']
+# Usage:
+# ruby get_box_metadadata.rb  --release alpine39 --distro generic
+# "3.6.12"
+# cd ~/Downloads
+# curl -L -o alpine39_new.box https://app.vagrantup.com/generic/boxes/alpine39/versions/3.6.12/providers/virtualbox.box
+#
+# minimal Vagrantfile:
+#  Vagrant.configure('2') do |config|
+#
+#    config.vm.box = 'generic/alpine39'
+#    boxname = 'alpine39.box'
+#    basedir = ENV.fetch('USERPROFILE', '')
+#    basedir = ENV.fetch('HOME', '') if basedir == ''
+#    basedir = basedir.gsub('\\', '/')
+#    config.vm.box_url = "file://#{basedir}/Downloads/#{boxname}"
+#    config.vm.network 'forwarded_port', guest: 80, host: 8080
+# end
+#
