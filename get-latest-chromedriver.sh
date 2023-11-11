cfc4bb30ba866a80cd31867f9555cdb43bf5c295
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-11-12 00:37:37 +0100

    intermediate revision - added quick download script (Linux)

---
+++ get-latest-chromedriver.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+PLATFORM='linux64'
+TMPFILE="/tmp/chrome-for-testing.$$.html"
+DRIVERFILE='/tmp/chromedriver-linux64.zip'
+URL='https://googlechromelabs.github.io/chrome-for-testing/'
+curl -sko $TMPFILE $URL
+echo "Examine $TMPFILE"
+VERSION=$(xmllint --htmlout --html --xpath "//div[@class='table-wrapper summary']/table/tbody/tr[th/a/text()='Stable']/td[1]/code/text()" $TMPFILE 2>/dev/null)
+echo "About to download chromedriver version $VERSION"
+URL=$(xmllint --htmlout --html --xpath "//section[@id='stable']/div[@class='table-wrapper']/table/tbody/tr[th/code='chromedriver' and td/code='200' and th/code='$PLATFORM']/td[1]/code/text()" $TMPFILE 2>/dev/null)
+echo "About to download chromedriver URL $URL"
+curl -sko $DRIVERFILE $URL
+echo "Verify contents of $DRIVERFILE"
+unzip -t $DRIVERFILE 
+#
+# https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/119.0.6045.105/linux64/chromedriver-linux64.zip
+
+rm -f $TMPFILE 
