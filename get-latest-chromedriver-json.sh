2e1098a11e555b461ca532a9afcf779e19304220
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-11-30 00:15:32 +0100

    intermediate revision - minor fix to jq chromedriver download url extractor script

--- get-latest-chromedriver-json.sh
+++ get-latest-chromedriver-json.sh
@@ -8,12 +8,14 @@ URL='https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versi
 DRIVERFILE='/tmp/chromedriver-linux64.zip'
 curl -sko $TMPFILE $URL
 
-# NOTE: the following query will show both chrome and chromedriver download links:
+# NOTE: the following query will show both chrome and chromedriver download links for Windows x64:  
 jq '.channels.Stable.downloads[][]|select(.platform=="win64").url'  $TMPFILE
 
 # the following query will show just the  chromedriver download link:
-jq '.channels.Stable.downloads.chromedriver[]|select(.platform=="win64").url' $TMPFILE
-DRIVERURL=$(jq '.channels.Stable.downloads.chromedriver[]|select(.platform=="win64").url' $TMPFILE)
+cat $TMPFILE | jq '.channels.Stable.downloads.chromedriver[]|select(.platform == "linux64")'
+
+PLATFORM='linux64'
+DRIVERURL=$(cat $TMPFILE | jq -cr ".channels.Stable.downloads.chromedriver[]|select(.platform == \"$PLATFORM\").url")
 echo "Will download URL $DRIVERURL"
 curl -sko $DRIVERFILE $DRIVERURL
 echo "Verify contents of $DRIVERFILE"
